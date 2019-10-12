a62dfcccb552e2f144ed421f14613540af389e2e
Steven Fuchs <stwf@mac.com>
2019-10-12 10:05:09 -0400

    Update mix.exs

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule Notsense.Mixfile do
   def project do
     [app: :notsense,
      version: "1.0.0",
-     elixir: ">= 1.5",
+     elixir: "~> 1.5",
      build_embedded: Mix.env == :prod,
      start_permanent: Mix.env == :prod,
 
